e6ef880259985a9bf2bec41226847d47fa9787c9
Jakub Kicinski <kuba@kernel.org>
2023-01-31 19:57:23 -0800

    tools: net: psp: add a connection test
    Add a test which tries to send and receive some traffic.
    
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- tools/net/ynl/psp.py
+++ tools/net/ynl/psp.py
@@ -5,6 +5,7 @@ import argparse
 import json
 import pprint
 import time
+import struct
 import socket
 
 from lib import YnlFamily
@@ -41,17 +42,84 @@ def test1(ynl):
     print()
 
 
+def spi_xchg(s, rx_spi):
+    s.send(struct.pack('I', rx_spi))
+    return struct.unpack('I', s.recv(4))[0]
+
+
+def test2(ynl):
+    devices = ynl.dev_get({}, dump=True)
+    dev = devices[0]
+
+    rx_assoc = ynl.rx_assoc_alloc({"version": 0, "dev-id": dev['id']})
+    rx_spi = rx_assoc['rx-key']['spi']
+    print('Local SPI:', rx_spi)
+
+    serv = socket.create_server(("", 1234), family=socket.AF_INET6, backlog=5,
+                                reuse_port=True, dualstack_ipv6=False)
+    while True:
+        (s, _) = serv.accept()
+
+        tx_spi = spi_xchg(s, rx_spi)
+        print('Remote SPI:', tx_spi)
+
+        assoc = ynl.assoc_add({"dev-id": dev['id'],
+                               "version": 0,
+                               "rx-key": rx_assoc['rx-key'],
+                               "tx-key": {'key': rx_assoc['rx-key']['key'],
+                                          'spi': tx_spi},
+                               "sock-fd": s.fileno()})
+
+        cnt = 0
+        while True:
+            n = s.recv(10000)
+            cnt += len(n)
+            if not n:
+                break
+            print('Received', cnt, end='\r')
+        print()
+
+
+def test3(ynl):
+    devices = ynl.dev_get({}, dump=True)
+    dev = devices[0]
+
+    rx_assoc = ynl.rx_assoc_alloc({"version": 0, "dev-id": dev['id']})
+    print('Local SPI:', rx_assoc['rx-key']['spi'])
+
+    s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+    s.connect(("db01::1:2", 1234))
+
+    tx_spi = spi_xchg(s, rx_assoc['rx-key']['spi'])
+    print('Remote SPI:', tx_spi)
+
+    assoc = ynl.assoc_add({"dev-id": dev['id'],
+                           "version": 0,
+                           "rx-key": rx_assoc['rx-key'],
+                           "tx-key": {'key': rx_assoc['rx-key']['key'],
+                                      'spi': tx_spi},
+                           "sock-fd": s.fileno()})
+
+    for i in range(100):
+        print(i, s.send(b'0123456789' * 200), end='\r')
+    print("Sent", (i + 1) * 2000)
+
+
 def main():
     parser = argparse.ArgumentParser(description='YNL sample')
     parser.add_argument('--spec', dest='spec', type=str, default='psp.yaml')
     parser.add_argument('--schema', dest='schema', type=str)
+    parser.add_argument('--no-schema', action='store_true')
     parser.add_argument('--json', dest='json_text', type=str)
     parser.add_argument('--do', dest='do', type=str)
     parser.add_argument('--dump', dest='dump', type=str)
     parser.add_argument('--sleep', dest='sleep', type=int)
     parser.add_argument('--subscribe', dest='ntf', type=str)
-    parser.add_argument('--test', dest='test', type=str)
+    parser.add_argument('--test', dest='test', type=int)
     args = parser.parse_args()
+
+    if args.no_schema:
+        args.schema = ''
 
     attrs = {}
     if args.json_text:
@@ -59,8 +127,12 @@ def main():
 
     ynl = YnlFamily(args.spec, args.schema)
 
-    if args.test is not None:
+    if args.test == 1:
         test1(ynl)
+    elif args.test == 2:
+        test2(ynl)
+    elif args.test == 3:
+        test3(ynl)
 
     if args.ntf:
         ynl.ntf_subscribe(args.ntf)
