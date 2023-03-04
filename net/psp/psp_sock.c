3f07fc33c32acb3789d0a8c9fc640c7a55280099
Jakub Kicinski <kuba@kernel.org>
2023-03-03 19:59:51 -0800

    psp: track generations of device key
    There is a (somewhat theoretical in absence of multi-host support)
    possibility that another entity will rotate the key and we won't
    know. This may lead to accepting packets with matching SPI but
    which used different crypto keys than we expected. Maintain and
    compare "key generation" per PSP spec.
    
    Since we're tracking "key generations" more explicitly now,
    maintain different lists for associations from different generations.
    This way we can catch stale associations (the user space should
    listen to rotation notifications and change the keys).
    
    Drivers can "opt out" of generation tracking by setting
    the generation value to 0.
    
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- net/psp/psp_sock.c
+++ net/psp/psp_sock.c
@@ -58,6 +58,7 @@ struct psp_assoc *psp_assoc_create(struct psp_dev *psd)
 		return NULL;
 
 	pas->psd = psd;
+	pas->generation = psd->generation;
 	psp_dev_get(psd);
 	refcount_set(&pas->refcnt, 1);
 
@@ -233,6 +234,21 @@ exit_clear_rx:
 exit_unlock:
 	release_sock(sk);
 	return err;
+}
+
+void psp_assocs_key_rotated(struct psp_dev *psd)
+{
+	struct psp_assoc *pas, *next;
+
+	/* Mark the stale associations as invalid, they will no longer
+	 * be able to Rx any traffic.
+	 */
+	list_for_each_entry_safe(pas, next, &psd->prev_assocs, assocs_list)
+		pas->generation |= ~PSP_GEN_VALID_MASK;
+	list_splice_init(&psd->prev_assocs, &psd->stale_assocs);
+	list_splice_init(&psd->active_assocs, &psd->prev_assocs);
+
+	/* TODO: we should inform the sockets that got shut down */
 }
 
 void psp_twsk_init(struct tcp_timewait_sock *tw, struct sock *sk)
