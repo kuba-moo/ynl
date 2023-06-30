6e95a46e6e9dd28484d4435da8b60ef738433297
Jakub Kicinski <kuba@kernel.org>
2023-06-30 16:48:07 -0700

    net: page_pool: hide page_pool_release_page()
    There seems to be no user calling page_pool_release_page()
    for legit reasons, all the users simply haven't been converted
    to skb-based recycling, yet. Convert them, update the docs,
    and unexport the function.
    
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -5413,7 +5413,7 @@ read_again:
 					priv->dma_conf.dma_buf_sz);
 
 			/* Data payload appended into SKB */
-			page_pool_release_page(rx_q->page_pool, buf->page);
+			skb_mark_for_recycle(skb);
 			buf->page = NULL;
 		}
 
@@ -5425,7 +5425,7 @@ read_again:
 					priv->dma_conf.dma_buf_sz);
 
 			/* Data payload appended into SKB */
-			page_pool_release_page(rx_q->page_pool, buf->sec_page);
+			skb_mark_for_recycle(skb);
 			buf->sec_page = NULL;
 		}
 
