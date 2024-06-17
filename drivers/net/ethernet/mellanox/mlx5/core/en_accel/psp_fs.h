1bf15a5ed6f16f4bc694b9b59a06845a5d127c23
Raed Salem <raeds@nvidia.com>
2024-06-17 10:46:19 +0300

    net/mlx5e: Configure PSP Rx flow steering rules
    Set the Rx PSP flow steering rule where PSP packet is identified and
    decrypted using the dedicated UDP destination port number 1000. If packet
    is decrypted then a PSP marker and syndrome are added to metadata so SW can
    use it later on in Rx data path.
    
    The rule is set as part of init_rx netdev profile implementation.
    
    Signed-off-by: Raed Salem <raeds@nvidia.com>
    Signed-off-by: Rahul Rameshbabu <rrameshbabu@nvidia.com>
    Change-Id: Ie1c7b2a84eaa0dfcc6505576e6e65f417ff1c301

--- drivers/net/ethernet/mellanox/mlx5/core/en_accel/psp_fs.h
+++ drivers/net/ethernet/mellanox/mlx5/core/en_accel/psp_fs.h
@@ -12,6 +12,8 @@ struct mlx5e_psp_fs *mlx5e_accel_psp_fs_init(struct mlx5e_priv *priv);
 void mlx5e_accel_psp_fs_cleanup(struct mlx5e_psp_fs *fs);
 int mlx5_accel_psp_fs_init_tx_tables(struct mlx5e_priv *priv);
 void mlx5_accel_psp_fs_cleanup_tx_tables(struct mlx5e_priv *priv);
+int mlx5_accel_psp_fs_init_rx_tables(struct mlx5e_priv *priv);
+void mlx5_accel_psp_fs_cleanup_rx_tables(struct mlx5e_priv *priv);
 #else
 static inline int mlx5_accel_psp_fs_init_tx_tables(struct mlx5e_priv *priv)
 {
@@ -19,5 +21,10 @@ static inline int mlx5_accel_psp_fs_init_tx_tables(struct mlx5e_priv *priv)
 }
 
 static inline void mlx5_accel_psp_fs_cleanup_tx_tables(struct mlx5e_priv *priv) { }
+static inline int mlx5_accel_psp_fs_init_rx_tables(struct mlx5e_priv *priv)
+{
+	return 0;
+}
+static inline void mlx5_accel_psp_fs_cleanup_rx_tables(struct mlx5e_priv *priv) { }
 #endif /* CONFIG_MLX5_EN_PSP */
 #endif /* __MLX5_PSP_FS_H__ */
